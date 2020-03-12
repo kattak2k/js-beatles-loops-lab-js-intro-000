b74c4cf756d13bfa2933200a4ff1c27ad4817df0
Ravi <kkiran.ror@gmail.com>
2020-03-12 03:19:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function theBeatlesPlay(musicians, instruments){
  let list = []
  for (i = 0; i <= musicians.length; i++ ) {
     list.push(`"${musicians[i]} plays ${instruments[i]}"`);
- }
- console.log(list)
- return list
+ };
+ console.log(list);
+ return list;
 }
 
 function johnLennonFacts(){
