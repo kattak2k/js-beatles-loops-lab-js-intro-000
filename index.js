8509e99f849cbf49a9dbadd0dcb8e1cc1496afa6
Ravi <kkiran.ror@gmail.com>
2020-03-12 03:16:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // add solution here
-function theBeatlesPlay(){
-
-
+function theBeatlesPlay(musicians, instruments){
+ let list = []
+ for (i = 0, i <= musicians.length, i++ ) {
+     list[i] = `${musicians[i]} plays ${instruments[i]}` 
+ }
+return list
+      
+ 
 }
 
 function johnLennonFacts(){
