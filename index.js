be24b26e6d590c6fc709bf42b3509ded24b82009
Ravi <kkiran.ror@gmail.com>
2020-03-12 03:24:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 function theBeatlesPlay(musicians, instruments){
  let list = []
  for (i = 0; i < musicians.length; i++ ) {
-    list.push(`${musicians[i]} plays ${instruments[i]}`);
+   list.push(`${musicians[i]} plays ${instruments[i]}`);
  };
- console.log(list);
  return list;
 }
 
